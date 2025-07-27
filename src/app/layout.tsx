fdb88193612b9a328c7fc45db16e16910351261a
7337475780 <tharunlingala6@gmail.com>
2025-07-27 11:58:21 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: "600",
+});
+
+export const metadata: Metadata = {
+  title: "WebGenie",
+  description:
+    "WebGenie is an AI-powered website builder that lets you create stunning, fully functional websites in minutes using just a text prompt—no coding needed.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
